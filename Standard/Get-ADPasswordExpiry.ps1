991b19f79947ca7d21fd22b437f9a430480ff7ad
Tino Holmberg <tino.holmberg1@gmail.com>
2025-06-21 12:35:51 +0300

    slight improvemengts

--- Standard/Get-ADPasswordExpiry.ps1
+++ Standard/Get-ADPasswordExpiry.ps1
@@ -1,16 +1,16 @@
 # Prompt the user to enter a username
-$username = Read-Host "Enter username"
+$Username = Read-Host "Enter username"
 
 # Retrieve the user object from Active Directory, including the password expiry time
-$user = Get-ADUser -Identity $username -Properties "msDS-UserPasswordExpiryTimeComputed"
+$User = Get-ADUser -Identity $Username -Properties "msDS-UserPasswordExpiryTimeComputed"
 
 # Check if the user was found
-if ($user) {
+if ($User) {
     # Convert the password expiry time from file time to a readable date format
-    $expiryDate = [datetime]::FromFileTime($user."msDS-UserPasswordExpiryTimeComputed")
+    $expiryDate = [datetime]::FromFileTime($User."msDS-UserPasswordExpiryTimeComputed")
     
     # Display the username and password expiration date
-    Write-Output "User: $($user.SamAccountName)"
+    Write-Output "User: $($User.SamAccountName)"
     Write-Output "Password expires on: $expiryDate"
 } else {
     # Display a message if the user was not found
