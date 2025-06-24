05e51c878d42b0d6abd91d80b66eb4eedfd6ea65
Tino Holmberg <tino.holmberg1@gmail.com>
2025-06-24 08:50:07 +0300

    changed some variables

--- Standard/Get-ADUserLastModified.ps1
+++ Standard/Get-ADUserLastModified.ps1
@@ -1,15 +1,15 @@
 # Prompt for the sAMAccountName of the user to search
-$SamAccountName = Read-Host "Enter the username (sAMAccountName) of the user"
+$Username = Read-Host "Enter the username (sAMAccountName) of the user"
 
 # Attempt to retrieve the user object from the current domain
 try {
-    $User = Get-ADUser -Filter {SamAccountName -eq $SamAccountName} -Properties whenChanged
+    $User = Get-ADUser -Filter {SamAccountName -eq $Username} -Properties whenChanged
 
     if ($User) {
         Write-Output "Username: $($User.SamAccountName)"
         Write-Output "Last modified: $($User.whenChanged)"
     } else {
-        Write-Output "User '$SamAccountName' was not found."
+        Write-Output "User '$Username' was not found."
     }
 }
 catch {
